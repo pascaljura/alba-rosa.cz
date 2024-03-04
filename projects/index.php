308076c3766a7eac92ab3f9cac0d96eed97f7c48
Pascal.Jura <104727003+pascaljura@users.noreply.github.com>
2024-03-04 20:17:43 +0100

    Přidání ikonek v navbaru

--- projects/index.php
+++ projects/index.php
@@ -13,11 +13,11 @@
 
   <!-- Menu -->
   <ul class="navbar">
-    <li><a href="../">Home</a></li>
-    <li><a href="#">Projects</a></li>
-    <li><a href="../games/">GameHub</a></li>
-    <li><button onclick="signup()">Sign Up</button></li>
-    <li><button onclick="login()">Login</button></li>
+    <li><a href="../"><i class="fas fa-home"></i> Home</a></li>
+    <li><a href="../projects/"><i class="fas fa-project-diagram"></i> Projects</a></li>
+    <li><a href="../games/"><i class="fas fa-gamepad"></i> GameHub</a></li>
+    <li><button onclick="signup()"><i class="fas fa-user-plus"></i> Sign Up</button></li>
+    <li><button onclick="login()"><i class="fas fa-sign-in-alt"></i> Login</button></li>
   </ul>
 
   <!-- Úvodní text -->
@@ -49,9 +49,9 @@
     <button onclick="signup()" class='svitch'>Sign Up</button>
   </div>
 
-  <!-- Přidat skripty nebo odkazy na skripty pro funkcionalitu -->
-  <script src="../assets/script.js">
-    
+  <script src="./assets/script.js"></script>
+  <script src="https://kit.fontawesome.com/865012b7e6.js" crossorigin="anonymous"></script>
+
   </script>
 
 </body>
