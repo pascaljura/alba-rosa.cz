327c43767f5633320201f1ac8fe6c736b61593dd
Pascal.Jura <104727003+pascaljura@users.noreply.github.com>
2024-03-04 18:50:02 +0100

    Přidání ikonky webu

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./assets/style.css">
-  <title>Your Website</title>
+  <link rel="shortcut icon" href="./assets/icon.png" type="image/x-icon">
+  <title>Alba-rosa.cz</title>
 </head>
 
 <body>
