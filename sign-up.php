715688a4f5bfddedd7b9530127700ffdf71be9df
Pascal.Jura <104727003+pascaljura@users.noreply.github.com>
2024-03-10 19:50:54 +0100

    Přidána funkce na blokování duplicitních účtů

--- sign-up.php
+++ sign-up.php
@@ -6,25 +6,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-    // Vložení nového uživatele do tabulky "users"
-    $insertUserQuery = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+    // Kontrola, zda e-mail již existuje
+    $checkEmailQuery = "SELECT * FROM users WHERE email='$email'";
+    $result = $conn->query($checkEmailQuery);
 
-    if ($conn->query($insertUserQuery) === TRUE) {
-        // Úspěšně vytvořeno
-        $lastUserId = $conn->insert_id;
-
-        // Vložení dat do tabulky "purpix"
-        $insertPurpixQuery = "INSERT INTO purpix (users_id) VALUES ('$lastUserId')";
-
-        if ($conn->query($insertPurpixQuery) === TRUE) {
-            // Přesměrování na login.php s předanými údaji
-            header("Location: login.php?username=$username&email=$email&password=" . $_POST['password']);
-            exit();
-        } else {
-            echo "Error inserting data into 'purpix' table: " . $conn->error;
-        }
+    if ($result->num_rows > 0) {
+        // E-mail již existuje v databázi
+        echo "E-mail already registered. Please use a different e-mail.";
     } else {
-        echo "Error inserting data into 'users' table: " . $conn->error;
+        // E-mail je unikátní, pokračujeme s vytvořením účtu
+        // Vložení nového uživatele do tabulky "users"
+        $insertUserQuery = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+        if ($conn->query($insertUserQuery) === TRUE) {
+            // Úspěšně vytvořeno
+            $lastUserId = $conn->insert_id;
+
+            // Vložení dat do tabulky "purpix"
+            $insertPurpixQuery = "INSERT INTO purpix (users_id) VALUES ('$lastUserId')";
+
+            if ($conn->query($insertPurpixQuery) === TRUE) {
+                // Přesměrování na login.php s předanými údaji
+                header("Location: login.php?username=$username&email=$email&password=" . $_POST['password']);
+                exit();
+            } else {
+                echo "Error inserting data into 'purpix' table: " . $conn->error;
+            }
+        } else {
+            echo "Error inserting data into 'users' table: " . $conn->error;
+        }
     }
 }
 
