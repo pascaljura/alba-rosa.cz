05c29243cdc05ac0b41a6dbea4223901afd595a1
Pascal.Jura <104727003+pascaljura@users.noreply.github.com>
2024-05-08 14:14:52 +0200

    Přidání statistiky na web

--- school-projects/index.php
+++ school-projects/index.php
@@ -101,7 +101,15 @@ if (isset($_SESSION['idusers'])) {
   echo '
   <footer><p style="color:white;"><a href="https://discord.gg/ZeHnED2BCG" target="_blank" style="color: white;"><i class="fa-brands fa-discord"></i></a> | Jiří Boucník &#38; Matěj Kořalka | &#169; 2024</p></footer>
   <script src="https://kit.fontawesome.com/865012b7e6.js" crossorigin="anonymous"></script>
-</body>
+  <a><script language="JavaScript" type="text/javascript" charset="utf-8">
+  <!--
+  document.write("<img src="https://toplist.cz/dot.asp?id=1811308&http="+
+  encodeURIComponent(document.referrer)+"&t="+encodeURIComponent(document.title)+"&l="+encodeURIComponent(document.URL)+
+  "&wi="+encodeURIComponent(window.screen.width)+"&he="+encodeURIComponent(window.screen.height)+"&cd="+
+  encodeURIComponent(window.screen.colorDepth)+"" width="1" height="1" border=0 alt="TOPlist" />");
+  //--></script><noscript><img src="https://toplist.cz/dot.asp?id=1811308&njs=1" border="0"
+  alt="TOPlist" width="1" height="1" /></noscript></a>
+  </body>
 </html>';
   exit();
 } else {
@@ -192,6 +200,14 @@ if (isset($_SESSION['idusers'])) {
 </div>
   <footer><p style="color:white;"><a href="https://discord.gg/ZeHnED2BCG" target="_blank" style="color: white;"><i class="fa-brands fa-discord"></i></a> | Jiří Boucník &#38; Matěj Kořalka | &#169; 2024</p></footer>
   <script src="https://kit.fontawesome.com/865012b7e6.js" crossorigin="anonymous"></script>
+  <a><script language="JavaScript" type="text/javascript" charset="utf-8">
+  <!--
+  document.write("<img src="https://toplist.cz/dot.asp?id=1811308&http="+
+  encodeURIComponent(document.referrer)+"&t="+encodeURIComponent(document.title)+"&l="+encodeURIComponent(document.URL)+
+  "&wi="+encodeURIComponent(window.screen.width)+"&he="+encodeURIComponent(window.screen.height)+"&cd="+
+  encodeURIComponent(window.screen.colorDepth)+"" width="1" height="1" border=0 alt="TOPlist" />");
+  //--></script><noscript><img src="https://toplist.cz/dot.asp?id=1811308&njs=1" border="0"
+  alt="TOPlist" width="1" height="1" /></noscript></a>
 </body>
 </html>';
   exit();
--- sign-up.php
+++ sign-up.php
@@ -24,3 +24,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 $conn->close();
 ?>
+<!DOCTYPE html>
+<html>
+    <body>
+    <a><script language="JavaScript" type="text/javascript" charset="utf-8">
+<!--
+document.write('<img src="https://toplist.cz/dot.asp?id=1811308&http='+
+encodeURIComponent(document.referrer)+'&t='+encodeURIComponent(document.title)+'&l='+encodeURIComponent(document.URL)+
+'&wi='+encodeURIComponent(window.screen.width)+'&he='+encodeURIComponent(window.screen.height)+'&cd='+
+encodeURIComponent(window.screen.colorDepth)+'" width="1" height="1" border=0 alt="TOPlist" />');
+//--></script><noscript><img src="https://toplist.cz/dot.asp?id=1811308&njs=1" border="0"
+alt="TOPlist" width="1" height="1" /></noscript></a>
+    </body>
+</html>
