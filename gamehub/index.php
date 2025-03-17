73fa71fdc75b5fd78c9639b8016c5d67dbc4a786
pascal.jura <boucnik.jiri@gmail.com>
2025-03-17 19:05:07 +0100

    oprava

--- gamehub/index.php
+++ gamehub/index.php
@@ -22,6 +22,8 @@ if (isset($_SESSION['idusers'])) {
   <!DOCTYPE html>
 <html lang="en">
 <head>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8345853981011942"
+     crossorigin="anonymous"></script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../assets/style.css">
